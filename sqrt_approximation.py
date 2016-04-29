5833735a2e5fb5167960eb03c8d98aade440e99b
Umar Mahmud <ujmahmud@gmail.com>
2016-04-29 15:31:12 -0400

    fixed naming

--- sqrt_approximation.py
+++ sqrt_approximation.py
@@ -9,7 +9,7 @@ while (abs(my_guess**2 - x)) >= epsilon and my_guess <= x:
 	my_guess += step
 	counter += 1
 
-print str(my_guess) + ' in ' + str(counter) + ' steps'
+print str(my_guess) + ' in ' + str(counter) + ' iterations'
 
 if (abs(my_guess**2 -x)) >= epsilon:
 	print 'unacceptable'
