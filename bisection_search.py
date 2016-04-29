a8559e3dbefe6a92deefadf246dfce74cd1f4d37
Umar Mahmud <ujmahmud@gmail.com>
2016-04-29 14:33:18 -0400

    commit

---
+++ bisection_search.py
@@ -0,0 +1,31 @@
+# Paste your code into this box
+ceiling = 100
+base = 0
+number = (ceiling + base) / 2
+answer = ''
+
+print 'Please think of a number between 0 and 100!'
+print 'Is your secret number ' + str(number) + '?'
+
+while (True):
+
+    answer = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+
+    if answer == 'l':
+        base = number
+        number = (ceiling + base) / 2
+    
+    elif answer == 'h':
+        ceiling = number
+        number = (ceiling + base) / 2
+        
+    elif answer == 'c':
+        break
+    
+    elif answer != 'l' or answer != 'h' or answer != 'c':
+        print ('Please enter valid choice')
+        
+    print 'Is your secret number ' + str(number) + '?'
+        
+print 'Game over. Your secret number was: ' + str(number) 
+
